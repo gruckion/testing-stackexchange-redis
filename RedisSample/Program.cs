b2be76082cca3059681362e633b4f42c882aaaec
Stephen Rayner <stephen.r.rayner@gmail.com>
2019-12-31 14:27:19 +0000

    init

---
+++ RedisSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisSample
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var redisCon = ConnectionMultiplexer.Connect("localhost");
+            var db = redisCon.GetDatabase();
+            db.StringSet("foo", "bar");
+
+            string val = db.StringGet("foo");
+
+            Console.WriteLine($"output: {val}");
+        }
+    }
+}
